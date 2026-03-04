c706d08e95b8e06bb95ed8a4bcfa07399b6095da
Amir <amiirr62@yahoo.com>
2026-03-03 22:52:33 -0800

    Fetch Database from BackEnd

--- Week07/Armaan-starter-code/frontend/src/App.tsx
+++ Week07/Armaan-starter-code/frontend/src/App.tsx
@@ -1,12 +1,22 @@
 import { Header } from "./components/Header";
 import { Assignments } from "./components/Assignments";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TAssignment } from "./interfaces";
 
 function App() {
   const [assignments, setAssignments] = useState<TAssignment[]>([]);
 
-  return (
+  // TODO: RUN LOGIC TO TALK TO BACKEND SERVER
+useEffect(() => {
+  async function getAssignment(){
+   const res = await  fetch("http://localhost:8000/assignments") 
+   const json = await res.json() 
+   setAssignments(json)
+  } 
+    getAssignment()
+}, []) 
+ 
+  return ( 
     <>
       <Header setAssignments={setAssignments} />
       <Assignments assignments={assignments} setAssignments={setAssignments} />
